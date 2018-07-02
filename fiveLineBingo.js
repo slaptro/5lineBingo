55a251d9e5652d4558399c62ee1b0a808f8bed6a
slaptro <leisureemail1@gmail.com>
2018-07-02 09:10:58 -0700

    added comments

--- fiveLineBingo.js
+++ fiveLineBingo.js
@@ -1,14 +1,16 @@
 for(i = 0; i < 25; i++){
 	document.getElementById("num_select").options[i] = new Option(i+1,i+1);
 }
-
+//generate an array of arrays of all possible lines of 5 numbers given a board of dim by dim
 function sumLines(dim){
 	var b = [];
 	var line = [];
 	var lineOut = [];
+	//generate the board
 	for(i = 1; i < dim*dim+1; i++){
 		b.push(i);
 	}
+	//generate all horizontal winning lines
 	for(i = 0; i < dim*dim; i += dim){
 		line = [];
 		for(j = 0; j < dim; j++){
@@ -16,6 +18,7 @@ function sumLines(dim){
 		}
 	lineOut.push(line);
 	}
+	//generate all vertical winning lines
 	for(i = 0; i < dim; i++){
 		line = [];
 		for(j = 0; j < dim*dim; j += dim){
@@ -23,6 +26,7 @@ function sumLines(dim){
 		}
 		lineOut.push(line);
 	}
+	//generate the 2 diagonal winning lines
 	line = [];
 	for(i = 0; i < dim*dim; i += dim+1){
 		line.push(b[i]);
@@ -35,12 +39,16 @@ function sumLines(dim){
 	lineOut.push(line);
 	return lineOut;
 }
-
-function combine(array,len,start){
+/*
+	using recursion, find all combinations of winning lines that will win the game
+		array = an array of arrays all possible winning lines from sumLines(dim)
+		len = the number of winning lines needed on a board to win (5 for 5 line bingo)
+*/
+function combine(array,len){
 	var result = [];
 	result.length = len;
 	var combined = [];
-
+	//recursive function to find all possible winning line combinations
 	function recursive(array,len,start) {
 		if(len === 0) {
 			combined.push(result.slice(0,result.length));
@@ -51,15 +59,15 @@ function combine(array,len,start){
 			recursive(array, len-1, i+1);
 		}
 	}
-
 	recursive(array, result.length, 0)
 	return combined;
 }
-
+//generate an array of arrays that list all unique numbers used in a certain set of winning lines
 function winBingo(array){
 	var concated = [];
 	var winBingo = [];
 	var tempArr = [];
+	//generate an array of arrays with all numbers used in each set of winning lines from combine(array,len)
 	for(i = 0; i < array.length; i++){
 		tempArr = array[i][0];
 		for(j = 1; j < array[i].length; j++){
@@ -67,6 +75,7 @@ function winBingo(array){
 		}
 		concated.push(tempArr);
 	}
+	//list all unique numbers used in each set of winning lines
 	for(i = 0; i < concated.length; i++){
 		tempArr = [];
 		for(j = 0; j < concated[i].length; j++){
@@ -78,7 +87,7 @@ function winBingo(array){
 	}
 	return winBingo;
 }
-
+//return all sets of winning lines of length len
 function getUnique(array,len){
 	var uniqueLen = [];
 	for(i = 0; i < array.length; i++){
@@ -88,16 +97,17 @@ function getUnique(array,len){
 	}
 	return uniqueLen;
 }
-
+//convert array of arrays arr into a board format with dimensions dim x dim
 function array2board(arr,dim){
 	var tempStr = "";
 	var tempArr1 = [];
 	var tempArr2 = [];
 	var starterArr = [];
+	//generate empty board
 	for(i = 1; i < dim*dim+1; i++){
 		starterArr.push("__");
 	}
-
+	//fill empty board using unique number array from winBingo(array)
 	for(i = 0; i < arr.length; i++){
 		tempArr1 = starterArr.slice(0,starterArr.length);
 		for(j = 0; j < dim*dim; j++){
@@ -105,7 +115,7 @@ function array2board(arr,dim){
 		}
 		tempArr2.push(tempArr1);
 	}
-
+	//convert array of arrays into a string that displays the results as a board format
 	var zero = "";
 	for(i = 0; i < tempArr2.length; i++){
 		tempStr += "["
@@ -130,14 +140,12 @@ function array2board(arr,dim){
 	}
 	return tempStr;
 }
-
+//run all functions and output a user friendly result to html file
 function Bingo(){
 	var num = document.getElementById("num_select").value;
 	var dim = 5;
 	var lines = sumLines(dim);
-
-	var winLines = winBingo(combine(lines, dim, 0));
-
+	var winLines = winBingo(combine(lines, dim));
 	var unique = getUnique(winLines,num);
 	var ways = array2board(unique,dim);
 	var plural = "";
